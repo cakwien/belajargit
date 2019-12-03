50e08b8a679f273b0404c33882b36d6544c46919
cakwien <58307142+cakwien@users.noreply.github.com>
2019-12-03 07:38:27 +0700

    update 1

--- belajargit.php
+++ belajargit.php
@@ -4,12 +4,6 @@
     </head>
     
     <body>
-<<<<<<< HEAD
-        Ini adalah halaman belajar git <br>
-        baris kedua belajar Git <br>
-        baris ketiga belajar Git <br>
-=======
-        Ini adalah halaman belajar git
->>>>>>> parent of c9de949... Update belajargit.php
+        update pertama <br>
     </body>
 </html>
